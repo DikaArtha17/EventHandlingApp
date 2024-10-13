607cb63dfb01784e364ed9dc5455a399822f38c5
MSI GAMING <arthdika17@gmail.com>
2024-10-13 09:35:15 +0700

    add

---
+++ app/src/main/java/Dikaartha/eventhandlingexample/MainActivity.kt
@@ -0,0 +1,33 @@
+package Dikaartha.eventhandlingexample
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
+import Dikaartha.eventhandlingexample.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnDisplayMessage.setOnClickListener {
+            Toast.makeText(
+                applicationContext, "Hello World",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+        binding.btnDisplayMessage.setOnLongClickListener {
+            Snackbar.make(binding.rootLayout, "Long Click",
+                Snackbar.LENGTH_LONG).show()
+            true
+        }
+    }
+}
